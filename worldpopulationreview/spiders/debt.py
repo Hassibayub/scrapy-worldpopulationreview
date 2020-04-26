1d23e50fa628ba724dd0703fe9112e4b6526a32c
hassibayub <hassibayub@gmail.com>
2020-04-26 17:55:27 +0500

    "initial commit"

---
+++ worldpopulationreview/spiders/debt.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class DebtSpider(scrapy.Spider):
+    name = 'debt'
+    allowed_domains = ['www.worldpopulationreview.com']
+    start_urls = ['https://www.worldpopulationreview.com/countries/countries-by-national-debt/']
+
+    def parse(self, response):
+        rows = response.xpath("//table[@class='datatableStyles__StyledTable-bwtkle-1 hOnuWY table table-striped']/tbody/tr") 
+
+        # //table[@class='datatableStyles__StyledTable-bwtkle-1 hOnuWY table table-striped']
+        # yield {
+        #     "rows": rows
+        # }
+
+        for row in rows:
+            counrty_name = row.xpath('.//td[1]/a[@href]/text()').get()
+            debt = row.xpath(".//td[2]/text()").get()
+            popluation = row.xpath(".//td[3]/text()").get()
+
+            yield{
+                "name: ": counrty_name, 
+                "debt: ": debt, 
+                "poplulation: ": popluation
+            }
+
+
